db7923513948d1b063aa8829a4e069c77705fd9c
Soumajit Roy <itssoumajit.alt@gmail.com>
2024-08-01 22:32:44 +0530

    proto1

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-database.js";
+import { getDatabase,
+         ref,
+         push,
+         onValue,
+         remove } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-database.js";
 
+         
 const firebaseConfig = {
-    databaseURL: process.env.DATABASE_URL
-}
+    databaseURL: "https://leads-tracker-app-81401-default-rtdb.asia-southeast1.firebasedatabase.app/"
+};
+
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app)
+const referenceInDB = ref(database, "leads")
 
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
-
-
 
 function render(leads) {
     let listItems = ""
@@ -28,10 +33,21 @@ function render(leads) {
     ulEl.innerHTML = listItems
 }
 
+onValue(referenceInDB, function(snapshot) {
+    const snapshotDoesExist = snapshot.exists()
+    if (snapshotDoesExist) {
+        const snapshotValues = snapshot.val()
+        const leads = Object.values(snapshotValues)
+        render(leads)
+    }
+})
+
 deleteBtn.addEventListener("dblclick", function() {
-    
+    remove(referenceInDB)
+    ulEl.innerHTML = ""
 })
 
 inputBtn.addEventListener("click", function() {
-    inputEl.value = ""
+    push(referenceInDB, inputEl.value)
+    inputEl.value = "" 
 })
